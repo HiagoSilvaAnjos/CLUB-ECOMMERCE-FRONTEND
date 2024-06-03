0f42f307bca94121e5570e7e69068fcf52ed5ad5
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-06-03 08:41:59 -0300

    feat: ensuring User Context is Update on onAuthStateChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
 
 import "./App.css";
 
@@ -7,12 +8,34 @@ import LoginPage from "./Pages/Login/Login.page";
 import SignUpPage from "./Pages/Sign-up/Sign-up.page";
 
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./config/firebase.config";
+import { auth, db } from "./config/firebase.config";
+import { UserContext } from "./contexts/user.context";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function App() {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user);
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext);
+
+  onAuthStateChanged(auth, async (user) => {
+    const isSigninOut = isAuthenticated && !user;
+    if (isSigninOut) {
+      return logoutUser();
+    }
+
+    const isSigninIn = !isAuthenticated && user;
+    if (isSigninIn) {
+      const querySpanshot = await getDocs(
+        query(collection(db, "users"), where("id", "==", user.uid))
+      );
+
+      const userFormFirestore = querySpanshot.docs[0]?.data();
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return loginUser(userFormFirestore as any);
+    }
   });
+
+  console.log(isAuthenticated);
+
   return (
     <>
       <BrowserRouter>
