a0f19966046e3277de854ed0ce840362ed29e65d
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-06-03 08:43:27 -0300

    feat: ensuring login, account creation and sign out button are show based on authentication state

--- src/Components/Header/Header.component.tsx
+++ src/Components/Header/Header.component.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
 import {
   HeaderContainer,
@@ -10,9 +11,12 @@ import {
 import { BsCart3 } from "react-icons/bs";
 import { auth } from "../../config/firebase.config";
 import { signOut } from "firebase/auth";
+import { UserContext } from "../../contexts/user.context";
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const { isAuthenticated } = useContext(UserContext);
 
   const handleExplorerClick = () => {
     navigate("/");
@@ -32,9 +36,15 @@ const Header = () => {
         <HeaderTitle onClick={handleExplorerClick}>CLUB CLOTHING</HeaderTitle>
         <HeaderItems>
           <HeaderItem onClick={handleExplorerClick}>Explorar</HeaderItem>
-          <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
-          <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
-          <HeaderItem onClick={handleSignUpClick}>Criar conta</HeaderItem>
+          {!isAuthenticated && (
+            <>
+              <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
+              <HeaderItem onClick={handleSignUpClick}>Criar conta</HeaderItem>
+            </>
+          )}
+          {isAuthenticated && (
+            <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
+          )}
           <HeaderItem>
             <BsCart3 size={25} />
             <span style={{ marginLeft: 5 }}>5</span>
